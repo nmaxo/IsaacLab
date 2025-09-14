91c5590e89f6241476e47992c1a06ff9f11369fd
Nikita <xisoniki@gmail.com>
2025-09-14 14:25:35 +0300

    Add custom model

---
+++ skrl_custom/models/custom/custom_model.py
@@ -0,0 +1,56 @@
+import torch
+import torch.nn as nn
+
+# import the skrl components to build the RL system
+from skrl.agents.torch.sac import SAC, SAC_DEFAULT_CONFIG
+from skrl.envs.loaders.torch import load_isaaclab_env
+from skrl.envs.wrappers.torch import wrap_env
+from skrl.memories.torch import RandomMemory
+from skrl.models.torch import DeterministicMixin, GaussianMixin, Model
+from skrl.resources.preprocessors.torch import RunningStandardScaler
+from skrl.trainers.torch import SequentialTrainer
+from skrl.utils import set_seed
+
+
+# define models (stochastic and deterministic models) using mixins
+class CustomActor(GaussianMixin, Model):
+    def __init__(self, observation_space, action_space, device, clip_actions=False,
+                 clip_log_std=True, min_log_std=-5, max_log_std=2):
+        Model.__init__(self, observation_space, action_space, device)
+        GaussianMixin.__init__(self, clip_actions, clip_log_std, min_log_std, max_log_std)
+        # Вычисляем num_observations из поля "policy" в словаре
+        if isinstance(observation_space, spaces.Dict):
+            if "policy" not in observation_space.spaces:
+                raise ValueError("observation_space must contain 'policy' key for CustomActor")
+            self.num_observations = observation_space.spaces["policy"].shape[0]
+        else:
+            self.num_observations = observation_space.shape[0]
+        self.net = nn.Sequential(nn.Linear(self.num_observations, 512),
+                                 nn.ReLU(),
+                                 nn.Linear(512, 256),
+                                 nn.ReLU(),
+                                 nn.Linear(256, self.num_actions),
+                                 nn.Tanh())
+        self.log_std_parameter = nn.Parameter(torch.zeros(self.num_actions))
+
+    def compute(self, inputs, role):
+        return self.net(inputs["states"]), self.log_std_parameter, {}
+
+class CustomCritic(DeterministicMixin, Model):
+    def __init__(self, observation_space, action_space, device, clip_actions=False):
+        Model.__init__(self, observation_space, action_space, device)
+        DeterministicMixin.__init__(self, clip_actions)
+        if isinstance(observation_space, spaces.Dict):
+            if "policy" not in observation_space.spaces:
+                raise ValueError("observation_space must contain 'policy' key for CustomActor")
+            self.num_observations = observation_space.spaces["policy"].shape[0]
+        else:
+            self.num_observations = observation_space.shape[0]
+        self.net = nn.Sequential(nn.Linear(self.num_observations + self.num_actions, 512),
+                                 nn.ReLU(),
+                                 nn.Linear(512, 256),
+                                 nn.ReLU(),
+                                 nn.Linear(256, 1))
+
+    def compute(self, inputs, role):
+        return self.net(torch.cat([inputs["states"], inputs["taken_actions"]], dim=1)), {}
